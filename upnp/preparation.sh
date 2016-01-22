fd4bcf4bfa93f402897788d40569da546c5cb1ee
emeric254 <emeric254@live.fr>
2016-01-22 19:29:35 +0100

    ajout parametre nom

--- upnp/preparation.sh
+++ upnp/preparation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PiNAME="RaspberryPi"
 
 # installation paquets
 apt-get install -y -q libupnp4 libupnp6 gmediarender gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-alsa
@@ -7,7 +8,7 @@ apt-get install -y -q libupnp4 libupnp6 gmediarender gstreamer1.0-plugins-base g
 
 # configuration /etc/default/gmediarender
 sed -i "/ENABLED=/c\ENABLED=1" /etc/default/gmediarender
-sed -i "/UPNP_DEVICE_NAME=/c\UPNP_DEVICE_NAME=\"RaspberryPi\"" /etc/default/gmediarender
+sed -i "/UPNP_DEVICE_NAME=/c\UPNP_DEVICE_NAME=\"$PiNAME\"" /etc/default/gmediarender
 sed -i "/INITIAL_VOLUME_DB=/c\INITIAL_VOLUME_DB=0" /etc/default/gmediarender
 
 
