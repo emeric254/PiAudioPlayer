4ff81cf0641baddcf691241ebc897dbfb37fae4e
emeric254 <emeric254@live.fr>
2016-02-20 13:30:32 +0100

    remove sudo

--- mopidy/preparation.sh
+++ mopidy/preparation.sh
@@ -3,7 +3,7 @@
 # basé sur ce projet : https://github.com/mopidy/mopidy
 
 # ajout clef du repo
-wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
+wget -q -O - https://apt.mopidy.com/mopidy.gpg | apt-key add -
 
 # repo :
 # pi 1
@@ -16,7 +16,7 @@ apt-get update -y -q
 apt-get install -y -q build-essential python-dev python-pip gstreamer0.10-plugins-bad gstreamer0.10-plugins-good gstreamer0.10-plugins-ugly gstreamer0.10-plugins-base
 #easy_install pip
 easy_install-2.7 pip
-sudo pip2 install --upgrade mopidy mopidy-soundcloud mopidy-dirble mopidy-dleyna mopidy-tunein mopidy-podcast mopidy-somafm mopidy-beets mopidy-podcast-itunes mopidy-scrobbler mopidy-youtube mopidy-mpris Mopidy_MFE Mopidy-AudioAddict Mopidy-Qsaver Mopidy-Mobile Mopidy-Banshee Mopidy-Notifier Mopidy-Simple-Webclient Mopidy-Moped Mopidy-InternetArchive Mopidy-Local-Images Mopidy-WebSettings Mopidy-GMusic Mopidy-MusicBox-Webclient configobj jinja2 pafy
+pip2 install --upgrade mopidy mopidy-soundcloud mopidy-dirble mopidy-dleyna mopidy-tunein mopidy-podcast mopidy-somafm mopidy-beets mopidy-podcast-itunes mopidy-scrobbler mopidy-youtube mopidy-mpris Mopidy_MFE Mopidy-AudioAddict Mopidy-Qsaver Mopidy-Mobile Mopidy-Banshee Mopidy-Notifier Mopidy-Simple-Webclient Mopidy-Moped Mopidy-InternetArchive Mopidy-Local-Images Mopidy-WebSettings Mopidy-GMusic Mopidy-MusicBox-Webclient configobj jinja2 pafy
 
 # recherche plugins
 #pip search mopidy
